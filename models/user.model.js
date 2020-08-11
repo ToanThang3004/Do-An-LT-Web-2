4e3eb4049faa01823a7391563f02fac83b9be8d9
quoctan342 <69149606+quoctan342@users.noreply.github.com>
2020-08-11 18:11:11 +0700

    Add files via upload

---
+++ models/user.model.js
@@ -0,0 +1,44 @@
+const db = require('../utils/db');
+
+const TBL_USERS = 'users';
+
+
+module.exports = {
+    all: function () {
+        return db.load(`select * from ${TBL_USERS}`);
+    },
+    add: function(entity) {
+        return db.add(TBL_USERS, entity);
+    },
+    singleByUserName: async function (username) {
+        const rows = await db.load(`select * from ${TBL_USERS} where username = '${username}'`);
+        if (rows.length === 0)
+          return null;
+    
+        return rows[0];
+    },
+    singleByEmail: async function (email) {
+        const rows = await db.load(`select * from ${TBL_USERS} where Email = '${email}'`);
+        if (rows.length === 0)
+          return null;
+    
+        return rows[0];
+    },
+    singleByPhone: async function (phone) {
+        const rows = await db.load(`select * from ${TBL_USERS} where Phone = '${phone}'`);
+        if (rows.length === 0)
+          return null;
+    
+        return rows[0];
+    },
+    singleByUserID: async function (id) {
+      const rows = await db.load(`select * from ${TBL_USERS} where UserID = '${id}'`);
+      if (rows.length === 0)
+        return null;
+  
+      return rows[0];
+    },
+    single: async function (username) {
+      return db.load(`select * from ${TBL_USERS} where username = ${username}`);
+    }
+};
