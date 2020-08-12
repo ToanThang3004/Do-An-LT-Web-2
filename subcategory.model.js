8b801a98d7028bfddecdf30313e98b20f7d4ce1d
ToanThang3004 <69257756+ToanThang3004@users.noreply.github.com>
2020-08-13 00:52:39 +0700

    code model cho subcategory

---
+++ subcategory.model.js
@@ -0,0 +1,46 @@
+const db = require('../utils/db');
+
+const TBL_SUBCATEGORIES = 'subcategories';
+
+module.exports = {
+    all: function () {
+        return db.load(`select * from ${TBL_SUBCATEGORIES}`);  
+    },
+    single: function (id) {
+        return db.load(`select * from ${TBL_SUBCATEGORIES} where CID = ${id}`);
+    },
+    singleforuser: function (id) {
+        return db.load(`select * from ${TBL_SUBCATEGORIES} where CID = ${id} and Del = 0`);
+    },
+    single2: function (id) {
+        return db.load(`select * from ${TBL_SUBCATEGORIES} where SCID = ${id}`);
+    },
+    add: function (entity) {
+        return db.add(TBL_SUBCATEGORIES, entity);
+    },
+    patch: function (entity) {
+        const condition = {
+          SCID: entity.SCID
+        }
+        delete entity.SCID;
+        return db.patch(TBL_SUBCATEGORIES, entity, condition);
+    },
+    del: function (id) {
+        const condition = {
+          SCID: id
+        };
+        const d = {
+          Del: 1
+        };
+        return db.patch(TBL_SUBCATEGORIES, d ,condition);
+    },
+    restore: function (id) {
+        const condition = {
+          SCID: id
+        };
+        const d = {
+          Del: 0
+        };
+        return db.patch(TBL_SUBCATEGORIES, d ,condition);
+    }
+}
