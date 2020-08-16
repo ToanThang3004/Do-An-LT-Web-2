784f59d4dc6edde3449706c9bbd519457e57e797
quoctan342 <69149606+quoctan342@users.noreply.github.com>
2020-08-16 19:31:43 +0700

    posts.model.js
    file code các model quản lí bài viết phân quyền quản trị viên (xóa, sửa, xem danh sách, thêm, di chuyển, khôi phục) (file hoàn chỉnh)

--- models/posts.model.js
+++ models/posts.model.js
@@ -6,6 +6,9 @@ module.exports = {
     all: function() {
         return db.load(`select * from ${TBL_POSTS}`)
     },
+    allByStatus: function(status) {
+        return db.load(`select * from ${TBL_POSTS} where Duyet = ${status}`)
+    },
     allPostSapPublic: function() {
         return db.load(`select * from ${TBL_POSTS} where Duyet = 2`)
     },
@@ -15,8 +18,14 @@ module.exports = {
     singleByCID: function (cid) {
         return db.load(`select * from ${TBL_POSTS} where CID = ${cid}`);
     },
+    singleByCIDStatus: function (cid, status) {
+        return db.load(`select * from ${TBL_POSTS} where CID = ${cid} and Duyet = ${status}`);
+    },
     singleByPostID: function (id) {
         return db.load(`select * from ${TBL_POSTS} where PostID = ${id}`);
+    },
+    singleByUserIDStatus: function (id, status) {
+        return db.load(`select * from ${TBL_POSTS} where UID = ${id} and Duyet = ${status}`);
     },
     singleBySCID: function (id) {
         return db.load(`select * from ${TBL_POSTS} where SCID = ${id}`);
@@ -40,5 +49,29 @@ module.exports = {
           SCID: scid
         };
         return db.patch(TBL_POSTS, d ,condition);
+    },
+    del: function (id) {
+        const condition = {
+          PostID: id
+        };
+        const d = {
+          xoa: 1
+        };
+        return db.patch(TBL_POSTS, d ,condition);
+    },
+    restore: function (id) {
+        const condition = {
+          PostID: id
+        };
+        const d = {
+          xoa: 0
+        };
+        return db.patch(TBL_POSTS, d ,condition);
+    },
+    del2: function (id) {
+      const condition = {
+        PostID: id
+      }
+      return db.del(TBL_POSTS, condition);
     }
 }
