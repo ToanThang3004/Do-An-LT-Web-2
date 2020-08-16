f63336145f99d164e0730ba8199a68c0d688a2bc
quoctan342 <69149606+quoctan342@users.noreply.github.com>
2020-08-16 19:52:02 +0700

    subcategory.route.js
    file code xử lí route link các chức năng quản lí chuyên mục con (thêm, xóa, sửa, xem danh sách, khôi phục) phân quyền quản trị viên (file hoàn chỉnh)

--- routes/subcategory.route.js
+++ routes/subcategory.route.js
@@ -5,107 +5,150 @@ const subcategoryModel = require('../models/subcategory.model');
 const router = express.Router();
 
 router.get('/:id', async function (req, res) {
-    const id = +req.params.id || -1;
-
-    const list = await subcategoryModel.single(id);
-    for (var i = 0; i < list.length; i++) {
-        if (list[i].Del == 1) {
-            list[i].Xoa = true;
-        } else {
-            list[i].Xoa = false;
+    if (req.isAuthenticated() && req.user.Permission === 3) {
+        const id = +req.params.id || -1;
+        const list = await subcategoryModel.single(id);
+        for (var i = 0; i < list.length; i++) {
+            if (list[i].Del == 1) {
+                list[i].Xoa = true;
+            } else {
+                list[i].Xoa = false;
+            }
         }
+        const rows = await categoryModel.single(id);
+        if (rows.length === 0)
+            return res.send('Invalid parameter.');
+        const category = rows[0];
+
+        res.render('vwSubCate/list', {
+            categories: list,
+            category,
+            xoa: list.Xoa === 1,
+            empty: list.length === 0
+        });
+    } else {
+        res.redirect('/')
     }
-    const rows = await categoryModel.single(id);
-    if (rows.length === 0)
-        return res.send('Invalid parameter.');
-    const category = rows[0];
-
-
-
-    res.render('vwSubCate/list', {
-        categories: list,
-        category,
-        xoa: list.Xoa === 1,
-        empty: list.length === 0
-    });
 })
 
 router.get('/:id/add', async function (req, res) {
-    const id = +req.params.id || -1;
-    const rows = await categoryModel.single(id);
-    if (rows.length === 0)
-      return res.send('Invalid parameter.');
-  
-    const category = rows[0];
-    res.render('vwSubCate/add', { category });
+    if (req.isAuthenticated() && req.user.Permission === 3) {
+        const id = +req.params.id || -1;
+        const rows = await categoryModel.single(id);
+        if (rows.length === 0)
+        return res.send('Invalid parameter.');
+    
+        const category = rows[0];
+        res.render('vwSubCate/add', { category });
+    } else {
+        res.redirect('/')
+    }
 })
 
 router.post('/:id/add', async function (req, res) {
-    const id = +req.params.id || -1;
-    const entity = {
-        SCName: req.body.SCName,
-        CID: id
-    };
-    console.log(entity);
-    await subcategoryModel.add(entity);
-    
-    res.redirect('/admin/subcategories/'+id);
+    if (req.isAuthenticated() && req.user.Permission === 3) {
+        const id = +req.params.id || -1;
+        const entity = {
+            SCName: req.body.SCName,
+            CID: id
+        };
+        await subcategoryModel.add(entity);
+        
+        res.redirect('/admin/subcategories/'+id);
+    } else {
+        res.redirect('/')
+    }
 })
 
 router.get('/edit/:id', async function (req, res) {
-    const id = +req.params.id || -1;
-    const rows = await subcategoryModel.single2(id);
-    if (rows.length === 0)
-      return res.send('Invalid parameter.');
-  
-    const subcategory = rows[0];
-    res.render('vwSubCate/edit', { subcategory });
+    if (req.isAuthenticated() && req.user.Permission === 3) {
+        const id = +req.params.id || -1;
+        const rows = await subcategoryModel.single2(id);
+        if (rows.length === 0)
+        return res.send('Invalid parameter.');
+    
+        const subcategory = rows[0];
+        res.render('vwSubCate/edit', { subcategory });
+    } else {
+        res.redirect('/')
+    }
 })
 
 router.post('/update', async function (req, res) {
-    const id = req.body.SCID;
-    await subcategoryModel.patch(req.body);
-    res.redirect('/admin/subcategories/edit/'+id);
-  })
+    if (req.isAuthenticated() && req.user.Permission === 3) {
+        const id = req.body.SCID;
+        await subcategoryModel.patch(req.body);
+        res.redirect('/admin/subcategories/edit/'+id);
+    } else {
+        res.redirect('/')
+    }
+})
 
 router.post('/del/:id', async function(req, res) {
-    const scid = req.body.SCID;
-    const subcatory = await subcategoryModel.single2(scid);
-    const cid = subcatory[0].CID;
-    await subcategoryModel.del(req.body.SCID);
+    if (req.isAuthenticated() && req.user.Permission === 3) {
+        const scid = req.body.SCID;
+        const subcatory = await subcategoryModel.single2(scid);
+        const cid = subcatory[0].CID;
+        await subcategoryModel.del(req.body.SCID);
 
-    res.redirect('/admin/subcategories/'+cid);
+        res.redirect('/admin/subcategories/'+cid);
+    } else {
+        res.redirect('/')
+    }
 })
   
 router.get('/del/:id', async function(req, res) {
-    const id = +req.params.id || -1;
-    const rows = await subcategoryModel.single2(id);
-    if(rows.length === 0) 
-        res.send('Invaild parameter.');
-    const cid = rows[0].CID;
+    if (req.isAuthenticated() && req.user.Permission === 3) {
+        const id = +req.params.id || -1;
+        const rows = await subcategoryModel.single2(id);
+        if(rows.length === 0) 
+            res.send('Invaild parameter.');
+        const cid = rows[0].CID;
 
-    await subcategoryModel.del(req.params.id);
-    res.redirect('/admin/subcategories/'+cid);
+        await subcategoryModel.del(req.params.id);
+        res.redirect('/admin/subcategories/'+cid);
+    } else {
+        res.redirect('/')
+    }
 })
 
 router.post('/restore/:id', async function(req, res) {
-    const scid = req.body.SCID;
-    const subcatory = await subcategoryModel.single2(scid);
-    const cid = subcatory[0].CID;
-    await subcategoryModel.restore(req.body.SCID);
+    if (req.isAuthenticated() && req.user.Permission === 3) {
+        const scid = req.body.SCID;
+        const subcatory = await subcategoryModel.single2(scid);
+        const cid = subcatory[0].CID;
+        await subcategoryModel.restore(req.body.SCID);
 
-    res.redirect('/admin/subcategories/'+cid);
+        res.redirect('/admin/subcategories/'+cid);
+    } else {
+        res.redirect('/')
+    }
 })
   
 router.get('/restore/:id', async function(req, res) {
-    const id = +req.params.id || -1;
-    const rows = await subcategoryModel.single2(id);
-    if(rows.length === 0) 
-        res.send('Invaild parameter.');
-    const cid = rows[0].CID;
+    if (req.isAuthenticated() && req.user.Permission === 3) {
+        const id = +req.params.id || -1;
+        const rows = await subcategoryModel.single2(id);
+        if(rows.length === 0) 
+            res.send('Invaild parameter.');
+        const cid = rows[0].CID;
 
-    await subcategoryModel.restore(req.params.id);
-    res.redirect('/admin/subcategories/'+cid);
+        await subcategoryModel.restore(req.params.id);
+        res.redirect('/admin/subcategories/'+cid);
+    } else {
+        res.redirect('/')
+    }
+})
+
+router.get('/delete/:id', async function (req, res) {
+    if (req.isAuthenticated() && req.user.Permission === 3) {
+        const id = +req.params.id || -1;
+        const cid = await subcategoryModel.single2(id);
+        await subcategoryModel.del2(id);
+        res.redirect('/admin/subcategories/'+cid[0].CID);
+    } else {
+        res.redirect('/')
+    }
 })
 
 module.exports = router;
