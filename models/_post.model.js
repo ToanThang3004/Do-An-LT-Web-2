668e045fb1c109884fa9d39b4a66191d37336bb3
quoctan342 <69149606+quoctan342@users.noreply.github.com>
2020-08-17 01:54:40 +0700

    Add files via upload
    file các model bài viết xem bởi tất cả phân quyền (file hoàn chỉnh)

--- models/_post.model.js
+++ models/_post.model.js
@@ -12,25 +12,31 @@ module.exports = {
     singleByUserID: function (id) {
         return db.load(`select * from ${TBL_POSTS} where UID = ${id}`);
     },
-    new: function (id) {
+    new: function () {
         return db.load(`select * from ${TBL_POSTS} where Duyet = 3 ORDER BY TimePost DESC`);
     },
-    new10: function (id) {
+    new1: function (id) {
+        return db.load(`select * from ${TBL_POSTS} where Duyet = 3 and CID = ${id} ORDER BY TimePost DESC LIMIT 1`);
+    },
+    new10: function () {
         return db.load(`select * from ${TBL_POSTS} where Duyet = 3 ORDER BY TimePost DESC LIMIT 10`);
     },
-    hot10: function (id) {
+    hot10: function () {
         return db.load(`select * from ${TBL_POSTS} where Duyet = 3 ORDER BY view DESC LIMIT 10`);
     },
-    hot: function (id) {
+    hot: function () {
         return db.load(`select * from ${TBL_POSTS} where Duyet = 3 ORDER BY view DESC`);
     },
     best: function() {
         return db.load(`select * from ${TBL_POSTS} where Duyet = 3 ORDER BY view DESC LIMIT 1`)
     },
-    hot2: function (id) {
+    hot2: function () {
         return db.load(`select * from ${TBL_POSTS} where Duyet = 3 ORDER BY view DESC LIMIT 4`);
     },
     upview: function (id) {
         return db.load(`UPDATE ${TBL_POSTS} SET view = view + 1 where PostID = ${id}`)
-    }
+    },
+    search: function (value) {
+        return db.load(`select DISTINCT * from ${TBL_POSTS} where PostTitle like N'%${value}%' OR SumContent like N'%${value}%' OR Content like N'%${value}%' ORDER BY Premium DESC`);
+    },
 }
