8bd437e5957d9d77a4b42e8d48a316493c818afb
ToanThang3004 <69257756+ToanThang3004@users.noreply.github.com>
2020-08-17 02:08:42 +0700

    update category.route.js
    update file xử lí các route link chức năng quản lí chuyên mục lớn ( thêm , xóa ,sửa, khôi phục, xem danh sách chuyên mục) (file hoàn chỉnh)

--- routes/category.route.js
+++ routes/category.route.js
@@ -4,74 +4,121 @@ const categoryModel = require('../models/category.model');
 const router = express.Router();
 
 router.get('/', async function (req, res) {
-    const list = await categoryModel.all();
+    if (req.isAuthenticated() && req.user.Permission === 3) {
+        const list = await categoryModel.all();
 
-    for (var i = 0; i < list.length; i++) {
-        if (list[i].Xoa === 1) {
-            list[i].del = true;
-        } else {
-            list[i].del = false;
+        for (var i = 0; i < list.length; i++) {
+            if (list[i].Xoa === 1) {
+                list[i].del = true;
+            } else {
+                list[i].del = false;
+            }
         }
-    }
 
-    res.render('vwCategories/list', {
-        categories: list,
-        empty: list.length === 0
-    });
+        res.render('vwCategories/list', {
+            categories: list,
+            empty: list.length === 0
+        });
+    } else {
+        res.redirect('/')
+    }
 })
 
 router.get('/add', function (req, res) {
-    res.render('vwCategories/add');
+    if (req.isAuthenticated() && req.user.Permission === 3) {
+        res.render('vwCategories/add');
+    } else {
+        res.redirect('/')
+    }
 })
   
 router.post('/add', async function (req, res) {
-    await categoryModel.add(req.body);
-    res.render('vwCategories/add');
+    if (req.isAuthenticated() && req.user.Permission === 3) {
+        await categoryModel.add(req.body);
+        res.render('vwCategories/add');
+    } else {
+        res.redirect('/')
+    }
 })
 
 router.post('/del/:id', async function(req, res) {
-    await categoryModel.delL(req.body.ID);
-    res.redirect('/admin/categories');
+    if (req.isAuthenticated() && req.user.Permission === 3) {
+        await categoryModel.delL(req.body.ID);
+        res.redirect('/admin/categories');
+    } else {
+        res.redirect('/')
+    }
 })
   
 router.get('/del/:id', async function(req, res) {
-    const id = +req.params.id || -1;
-    const rows = await categoryModel.single(id);
-    if(rows.length === 0) 
-        res.send('Invaild parameter.');
+    if (req.isAuthenticated() && req.user.Permission === 3) {
+        const id = +req.params.id || -1;
+        const rows = await categoryModel.single(id);
+        if(rows.length === 0) 
+            res.send('Invaild parameter.');
 
-    await categoryModel.delL(req.params.id);
-    res.redirect('/admin/categories');
+        await categoryModel.delL(req.params.id);
+        res.redirect('/admin/categories');
+    } else {
+        res.redirect('/')
+    }
 })
 
 router.post('/restore/:id', async function(req, res) {
-    await categoryModel.restoreL(req.body.ID);
-    res.redirect('/admin/categories');
+    if (req.isAuthenticated() && req.user.Permission === 3) {
+        await categoryModel.restoreL(req.body.ID);
+        res.redirect('/admin/categories');
+    } else {
+        res.redirect('/')
+    }
 })
   
 router.get('/restore/:id', async function(req, res) {
-    const id = +req.params.id || -1;
-    const rows = await categoryModel.single(id);
-    if(rows.length === 0) 
-        res.send('Invaild parameter.');
+    if (req.isAuthenticated() && req.user.Permission === 3) {
+        const id = +req.params.id || -1;
+        const rows = await categoryModel.single(id);
+        if(rows.length === 0) 
+            res.send('Invaild parameter.');
 
-    await categoryModel.restoreL(req.params.id);
-    res.redirect('/admin/categories');
+        await categoryModel.restoreL(req.params.id);
+        res.redirect('/admin/categories');
+    } else {
+        res.redirect('/')
+    }
 })
 
 router.get('/edit/:id', async function (req, res) {
-    const id = +req.params.id || -1;
-    const rows = await categoryModel.single(id);
-    if (rows.length === 0)
-      return res.send('Invalid parameter.');
-  
-    const category = rows[0];
-    res.render('vwCategories/edit', { category });
+    if (req.isAuthenticated() && req.user.Permission === 3) {
+        const id = +req.params.id || -1;
+        const rows = await categoryModel.single(id);
+        if (rows.length === 0)
+        return res.send('Invalid parameter.');
+    
+        const category = rows[0];
+        res.render('vwCategories/edit', { category });
+    } else {
+        res.redirect('/')
+    }
 })
 
 router.post('/update', async function (req, res) {
-    await categoryModel.patch(req.body);
-    res.redirect('/admin/categories');
+    if (req.isAuthenticated() && req.user.Permission === 3) {
+        const id = req.body.CID;
+        await categoryModel.patch(req.body);
+        res.redirect('/admin/categories#'+id);
+    } else {
+        res.redirect('/')
+    }
+})
+
+router.get('/delete/:id', async function (req, res) {
+    if (req.isAuthenticated() && req.user.Permission === 3) {
+        const id = +req.params.id || -1;
+        await categoryModel.del(id);
+        res.redirect('/admin/categories');
+    } else {
+        res.redirect('/')
+    }
 })
 
 module.exports = router;
